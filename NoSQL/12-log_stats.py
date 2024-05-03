722e8155447232489482306723019aa465c5e074
Efrén Jiménez <40528724+Efrenjm@users.noreply.github.com>
2024-05-03 01:27:36 -0600

    change string formatting

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -9,14 +9,15 @@ if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx_collection = client.logs.nginx
 
-    n_logs = nginx_collection.count_documents({})
+    logs_count = nginx_collection.count_documents({})
+    print("{} logs".format(logs_count))
 
+    print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    print('Methods:')
     for method in methods:
-        count = nginx_collection.count_documents({"method": method})
-        print(f'\tmethod {method}: {count}')
+        print("\tmethod {}: {}".format(
+            method, nginx_collection.count_documents({"method": method})))
 
-    status_check = nginx_collection.count_documents(
-        {"method": "GET", "path": "/status"})
-    print(f'{status_check} status check')
+    print("{} status check".format(nginx_collection.count_documents(
+        {"method": "GET", "path": "/status"})))
+    
