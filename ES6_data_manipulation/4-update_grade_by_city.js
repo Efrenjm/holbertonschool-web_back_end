bb5900ab4d5a09b49a45920b573eee4158781f69
Efrén Jiménez <40528724+Efrenjm@users.noreply.github.com>
2024-06-07 17:07:47 -0600

    create new files

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,12 +1,12 @@
-export default function updateStudentGradeByCity(students, city, newGrade) {
+export default function updateStudentGradeByCity(students, city, newGrades) {
   students
     .filter((student) => student.location === city)
     .map((item) => {
       const newRecord = { ...item };
 
-      const newStudent = newGrade.find((student) => student.studentId === item.id);
-      if (newStudent) newRecord.grade = newStudent.grade;
-      else newRecord.grade = 'N/A';
+      const newStudent = newGrades.find((student) => student.studentId === item.id);
+      newRecord.grade = newStudent ? newStudent.grade : 'N/A';
+
       return newRecord;
     });
 }
